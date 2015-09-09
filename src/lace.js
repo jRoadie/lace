c764796dba59c4d3bc276f796aaa159ae03fb898
Me <akter.hossain.ict@gmail.com>
2015-09-09 17:31:45 +0600

    initializing the awesome!

---
+++ src/lace.js
@@ -0,0 +1,21 @@
+(function() {
+
+    'use strict';
+
+    var lace,
+        version = '1.0.0';
+
+    var Lace = function() {
+
+    };
+
+    Lace.prototype = {
+
+        version: version,
+
+        constructor: Lace
+
+    }
+
+
+})();
